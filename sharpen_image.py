ef4ac9dd4ea9e23d260b5d157d61b62e32a817c0
Daniel S. Katz <d.katz@ieee.org>
2020-05-19 11:01:53 -0500

    Add files via upload

---
+++ sharpen_image.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageFilter
+import sys
+
+if len(sys.argv) != 2:
+    print("error - usage:", sys.argv[0], "basename")
+    sys.exit(2)
+
+basename = sys.argv[1]
+input_filename = basename + '.jpg'
+output_filename = basename + '_sharp.jpg'
+
+#Read image
+try:
+    im = Image.open(input_filename)
+except OSError:
+    print("error - can't open file: ", basename, ".jpg", sep="")
+    sys.exit(2)
+
+
+#Applying a filter to the image
+#im_sharp = im.filter(ImageFilter.SHARPEN)
+
+#Saving the filtered image to a new file
+#im_sharp.save(output_filename, 'JPEG')
